75f8fc4e9340e8e72895e4040ba13d985ec5bc81
= <=>
2025-09-18 14:11:52 -0500

    style: adjust padding in ContextMenu MenuButton for improved appearance

--- client/nodular/src/app/components/ContextMenu.tsx
+++ client/nodular/src/app/components/ContextMenu.tsx
@@ -28,7 +28,7 @@ export function ContextMenu({
     return (
         <Menu as="div" className={`relative ${className} flex flex-col gap-2 w-full`}>
             <label htmlFor="ctxmenu" className="text-sm font-medium text-slate-300">Memory Management</label>
-            <MenuButton id="ctxmenu" className="inline-flex w-full items-center justify-between gap-x-1.5 rounded-xl bg-slate-900 px-3 py-2 text-xs font-semibold text-white inset-ring-1 inset-ring-white/5 hover:bg-slate-950">
+            <MenuButton id="ctxmenu" className="inline-flex w-full items-center justify-between gap-x-1.5 p-1 rounded-xl bg-slate-900 px-3 py-3 text-xs font-semibold text-white inset-ring-1 inset-ring-white/5 hover:bg-slate-950">
                 <span className="truncate">{current}</span>
                 <ChevronDown size={16} className="-mr-1 text-gray-400" />
             </MenuButton>
