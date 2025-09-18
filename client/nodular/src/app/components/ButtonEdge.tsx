4c11d887fa2081d59d52392fe4a5bfb669351f49
jay kuroor <jaykuroor@outlook.com>
2025-09-18 00:38:13 -0500

    feat: add ButtonEdge component with interactive button and update styles for ChatBubbleNode and globals

---
+++ client/nodular/src/app/components/ButtonEdge.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  BaseEdge,
+  EdgeLabelRenderer,
+  getSmoothStepPath,
+  useReactFlow,
+  type EdgeProps,
+} from 'reactflow';
+import { X } from 'lucide-react';
+
+export default function ButtonEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  markerEnd,
+  data,
+}: EdgeProps) {
+  const [edgePath, labelX, labelY] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
+  const onEdgeClick = () => {
+    data.onEdgeClick(id);
+  };
+
+  return (
+    <>
+      <BaseEdge path={edgePath} markerEnd={markerEnd} style={style} />
+      <EdgeLabelRenderer>
+        <div
+          className="nodrag nopan"
+          style={{
+            position: 'absolute',
+            transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+            pointerEvents: 'all',
+          }}
+        >
+          <button className="button-edge__button" onClick={onEdgeClick}>
+            <X size={14} />
+          </button>
+        </div>
+      </EdgeLabelRenderer>
+    </>
+  );
+}
