50581a247714e90d931a8b0c44b0fccdbc7158f9
= <=>
2025-09-18 11:33:55 -0500

    feat: add Watermark component to display branding information

---
+++ client/nodular/src/app/components/Watermark.tsx
@@ -0,0 +1,7 @@
+export default function Watermark() {
+    return (
+        <div className="fixed group bottom-30 right-10 z-10 text-lg text-slate-100/50 select-none hover:text-slate-50/60">
+            Made with <a href="https://reactflow.dev/" target="_blank" className="font-medium text-slate-50/80 hover:underline hover:text-white group-hover:text-white">ReactFlow</a>
+        </div>
+    );
+}
